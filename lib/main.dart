0ad5749dc41567b6b56976f9ba51db6f2098b97a
iomullony <isabel@rugania.es>
2023-11-09 20:01:28 +0100

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:medrecs/views/userView/LoginScreen.dart';
+import 'package:medrecs/views/userView/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'configs/firebase_options.dart';
 
