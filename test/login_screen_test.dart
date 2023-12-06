829372d51543fd68f5af507d26e43972a63cd175
burakalpyel <burakalpyel@gmail.com>
2023-12-06 21:07:33 +0100

    Test

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -9,6 +9,12 @@ void main() {
         home: LoginScreen(), // Create an instance of LoginScreen
       ),
     );
+    //This test checks if the LoginScreen widget properly displays an error message when the password is wrong.
+    //It first creates the LoginScreen widget and finds its state to interact with it.
+    //Then, it triggers the display of the error message for an invalid password.
+    // The test verifies that the error message appears correctly and has an 'OK' button.
+    //It simulates tapping the 'OK' button to dismiss the error message.
+    // After that, it checks if the error message disappears as expected.
 
     // Get the state of the LoginScreen
     final loginScreenState = tester.state<LoginScreenState>(find.byType(LoginScreen));
@@ -16,7 +22,7 @@ void main() {
     // Call the _showInvalidPasswordDialog method
     loginScreenState.showInvalidPasswordDialog();
 
-    await tester.pump(); // Rebuild the widget after calling the method
+    await tester.pump();
 
     // Verify that the dialog is displayed
     expect(find.text('Invalid Password'), findsOneWidget);
@@ -33,7 +39,9 @@ void main() {
     expect(find.text('Invalid Password'), findsNothing);
     expect(find.text('The password is incorrect. Please try again.'), findsNothing);
   });
-
+  // this test ensures the proper display of a dialog in case of empty fields.
+  //After triggering the dialog, it rebuilds the widget and verifies the presence of specific text elements.
+  //Such as ('Empty Fields' and 'Please fill in both UserID and Password fields.') along with the 'OK' button within the dialog.
   testWidgets('Show Empty Field Dialog Test', (WidgetTester tester) async {
     await tester.pumpWidget(
       const MaterialApp(
--- test/med_screen_test.dart
+++ test/med_screen_test.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:medrecs/util/model/patientinfo.dart';
 import 'package:medrecs/views/medView/MedTeamScreen.dart';
+import 'package:medrecs/views/medView/AppointmentFormScreen.dart';
 
 void main() {
-  testWidgets('Widget Initialization Test', (WidgetTester tester) async {
+  testWidgets('Widget Initialization Test - MedTeamScreen', (WidgetTester tester) async {
     // Build our MedTeamScreen widget and trigger a frame
-    final widget = MedTeamScreen(
+    final medTeamScreenWidget = MedTeamScreen(
       userID: 1, // replace with appropriate user ID for testing
       userInfo: PatientInfo(name: '', surname: '', birthday: '', address: '', location: '', phone: '', medteamstatus: true, password: ''), // replace with appropriate PatientInfo object for testing
     );
-    await tester.pumpWidget(MaterialApp(home: widget));
+    await tester.pumpWidget(MaterialApp(home: medTeamScreenWidget));
 
     // Wait for all asynchronous operations to complete
     await tester.pumpAndSettle();
 
-    // Verify the initial state of certain UI elements
-    expect(find.text("${widget.userInfo.name} ${widget.userInfo.surname}'s Area"), findsOneWidget);
+    // Verify the initial state of certain UI elements in MedTeamScreen
+    expect(find.text("${medTeamScreenWidget.userInfo.name} ${medTeamScreenWidget.userInfo.surname}'s Area"), findsOneWidget);
     expect(find.text("Personal Details"), findsOneWidget);
     expect(find.byType(BottomNavigationBar), findsOneWidget);
     expect(find.byType(InkWell), findsNWidgets(2)); // Number of InkWells
+  });
+
+  testWidgets('Appointment Form Screen Renders Correctly', (WidgetTester tester) async {
+    // Build the AppointmentFormScreen widget and trigger a frame
+    final appointmentFormScreenWidget = AppointmentFormScreen();
+    await tester.pumpWidget(MaterialApp(home: appointmentFormScreenWidget));
+
+    // Expectations for AppointmentFormScreen initialization
+    expect(find.text('Appointment Form'), findsOneWidget);
+    expect(find.byType(TextFormField), findsNWidgets(6)); // Number of TextFormFields
+    expect(find.byType(ElevatedButton), findsOneWidget);
   });
 }
